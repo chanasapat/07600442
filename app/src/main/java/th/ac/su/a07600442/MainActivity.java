aaa6c3f8c1c5a1260bc6f34af5a146f092346c7e
chanasapat janha <chanasapat@gmail.com>
2020-08-29 23:34:51 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/su/a07600442/MainActivity.java
@@ -0,0 +1,14 @@
+package th.ac.su.a07600442;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
